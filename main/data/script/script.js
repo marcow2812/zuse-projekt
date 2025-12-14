ba1f1307cb14193dae59d3e82f0ef9c0489d7f70
Marco <marco.weber@konrad-zuse-schule.de>
2025-12-14 10:14:59 +0100

    Updated

--- main/data/script/script.js
+++ main/data/script/script.js
@@ -7,6 +7,20 @@ var standardUrl = "https://marcow2812.github.io/zuse-projekt/main/explorer.html?
 var objectId;
 
 
+const users = [
+      { id: 1, name: "Max Mustermann", alter: 28, beruf: "Entwickler" },
+      { id: 2, name: "Erika Musterfrau", alter: 34, beruf: "Designerin" },
+      { id: 3, name: "Hans Wurst", alter: 45, beruf: "Projektmanager" }
+    ];
+
+    console.log(users[0]);
+    
+    console.log(users[0].name);
+
+    console.log(users.find(u => u.id === 2).name);
+
+
+
 function loadThePage()
 {
     setObject("disco-1");
@@ -17,6 +31,18 @@ function loadThePage()
 
     openWelcomeModal();
 
+    /*
+    var valueForInnerHtml = "";
+
+    for (i = 0; i < 12; i++)
+    {
+        valueForInnerHtml = valueForInnerHtml + "<p>This is great</p>";
+    }
+
+    document.getElementById("testInnerHtml").innerHTML = valueForInnerHtml;
+    */
+
+
     setTimeout(function()
     {
         document.getElementById("loadingPage").style.display = "none";
@@ -24,7 +50,7 @@ function loadThePage()
     }, 3500)
 }
 
-
+/* --- GETTER-METHODEN --- */
 
 function getParameter()
 {
@@ -47,6 +73,7 @@ function getStandardUrl()
     return standardUrl;
 }
 
+/* --- SETTER-METHODEN --- */
 
 /*
 function setCanvasSize()
@@ -314,3 +341,99 @@ function normalize(corners, width, height) {
     return normalized;
 }
 
+
+
+
+
+
+var oSrc;
+var oTitle;
+var oBackground;
+var oSizeMultiplier;
+var oStartAudioAt;
+
+function getObjectTitle(parameter)
+{
+    setObject(parameter);
+    return oTitle;
+}
+
+function setObject(objectId)
+{
+    closeObjectMenu(); // Wenn von Menü aufgerufen
+
+    var model = document.getElementById("model");
+
+    switch (objectId)
+    {
+        case "heart-1":
+            oSrc = "./data/3d/heart-23.glb";
+            oTitle = "Rotes Herz";
+            oSizeMultiplier = 3;
+            oBackground = "rgb(255, 179, 179, 0.3)";
+            oStartAudioAt = 5;
+            break;
+        case "disco-1":
+            oSrc = "./data/3d/disco-ball-2730.glb";
+            oTitle = "Disco-Kugel";
+            oSizeMultiplier = 3;
+            oBackground = "transparent";
+            oStartAudioAt = 1.5;
+            break;
+        case "atom-1":
+            oSrc = "./data/3d/atom.glb";
+            oTitle = "Aufbau eines Atoms";
+            oSizeMultiplier = 4;
+            oBackground = "black";
+            oStartAudioAt = 5;
+            break;
+        case "city-1":
+            oSrc = "./data/3d/city_pack_5.glb";
+            oTitle = "Stadt";
+            oSizeMultiplier = 4;
+            oBackground = "#e6e6e6";
+            oStartAudioAt = 5;
+            break;
+        case "davinci-1":
+            oSrc = "./data/3d/da_vinci_code_cryptex.glb";
+            oTitle = "Da Vinci Cryptex";
+            oSizeMultiplier = 3;
+            oBackground = "transparent";
+            oStartAudioAt = 5;
+            break;
+        case "enigma-1":
+            oSrc = "./data/3d/enigma_machine_1934.glb";
+            oTitle = "Enigma";
+            oSizeMultiplier = 3;
+            oBackground = "url('./data/img/favicon.png')"; // black
+            oStartAudioAt = 5;
+            break;
+    }
+
+    // Werte auf Standard zurücksetzen
+    model.style.backgroundColor = "transparent";
+
+    // Zuweisung und Anwendung
+    model.src = oSrc;
+    document.getElementById("objectTitle").innerHTML = oTitle;
+    setAudioPlayTime(oStartAudioAt);
+    
+    // variable.includes("url(")
+
+    if (oBackground.includes("url("))
+    {
+        model.style.backgroundImage = oBackground;
+    }
+    else
+    {
+        model.style.backgroundColor = oBackground;
+    }    
+    
+}
+
+function setObjectSize(pixelSize)
+{
+    // console.log(pixelSize);
+    document.getElementById("model").style.width = (pixelSize * oSizeMultiplier) + "px";
+    document.getElementById("model").style.height = (pixelSize * oSizeMultiplier) + "px";
+}
